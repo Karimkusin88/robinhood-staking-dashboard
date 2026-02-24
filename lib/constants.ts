ef5679b349de7abc2154c4f53d146665182c3896
Karimkusin88 <karimkusin@gmail.com>
2026-02-25 01:51:31 +0700

    chore: add alpha constants (explorer, chainId, links)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,15 @@
-export const TOKEN_ADDRESS = "0x590b564b01C59C9a181c7a0694403e1DAB2fba16"
-export const NFT_ADDRESS = "0x265a208eE43A8dBc11BA8f28629D8eE377a31830"
-export const STAKING_ADDRESS = "0x989414a69D76ec173AE59D427D954699950eB336"
+export const CHAIN_ID = 46630;
+
+export const EXPLORER_BASE = "https://explorer.testnet.chain.robinhood.com";
+export const EXPLORER_ADDRESS = `${EXPLORER_BASE}/address/`;
+export const EXPLORER_TX = `${EXPLORER_BASE}/tx/`;
+
+export const TOKEN_ADDRESS = "0x590b564b01C59C9a181c7a0694403e1DAB2fba16";
+export const NFT_ADDRESS   = "0x265a208eE43A8dBc11BA8f28629D8eE377a31830";
+export const STAKING_ADDRESS = "0x98941a6907e6ec173AE59D427D954699950eB336";
+
+export const TOKEN_SYMBOL = "KRM";
+export const TOKEN_DECIMALS = 18;
+
+export const ROBINHOOD_GLOBAL_FAUCET = "https://faucet.testnet.chain.robinhood.com/";
+export const BUILDER_X = "https://x.com/Karimkusin";
